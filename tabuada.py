25242d1085db2ebaf90e8e4e7d65a11134c9e6b2
Diego Amorim <diegoamorim9999@hotmail.com>
2023-12-30 20:03:21 -0300

    nova formatacao de texto

--- tabuada.py
+++ tabuada.py
@@ -2,16 +2,26 @@
 
 """Imprimindo a tabuada do 1 ao 10."""
 
-__version_ = "0.0.1"
+__version_ = "0.1.1"
 __author__ = "Diego Amorim"
+
+template_base = """
+---Tabuada do 1---
+
+    {operacoes}
+
+#################
+"""
 
 
 #numeros = [1,2,3,4,5,6,7,8,9,10]
 numeros = list(range(1,255))
 
 # print(numeros)
-for numero in numeros:
-    print("Tabuada do :", numero)
-    for key in numeros:
-        print(key, "x", numero, ":", (key * numero) )
-print("===========================")
+for n1 in numeros:
+    for n2 in numeros:
+        operacao = f"{n1}x{n2} = {n1 * n2}"
+        print(operacao)
+    #print(template_base.format(
+    #    operacoes=operacoes
+    #))
